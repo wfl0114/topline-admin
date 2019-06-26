c746cc2a8cf1722c1b1ae4fc6a01169354194062
wfl <wang@123.com>
2019-06-26 21:47:20 +0800

    安装与配置element

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
     <p>App Component</p>
+    <el-row>
+      <el-button round>圆角按钮</el-button>
+      <el-button type="primary" round>主要按钮</el-button>
+      <el-button type="success" round>成功按钮</el-button>
+      <el-button type="info" round>信息按钮</el-button>
+      <el-button type="warning" round>警告按钮</el-button>
+      <el-button type="danger" round>危险按钮</el-button>
+    </el-row>
   </div>
 </template>
 
