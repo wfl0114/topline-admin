c94f663008903b21ad76a30d8828426f33d649cd
wfl0114 <17621858557@163.com>
2019-11-02 04:11:08 +0800

    用户登录-页面布局

--- src/main.js
+++ src/main.js
@@ -2,16 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-
 // 优先查找文件，如果文件找不到则找目录
 // 找到目录,优先加载目录中的index.js
 import router from './router'
-
+import './styles/common.css'
 Vue.use(ElementUI)
 
-Vue.config.productionTip = false
-
 // http://ttapi.research.itcast.cn/mp/
+
+Vue.config.productionTip = false
 
 new Vue({
   router,
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,35 @@
 <template>
-  <div>
-    <p>Login Component</p>
+  <div class="login_wrap">
+    <div class="login_form_wrap">
+      <div class="login_head">
+        <img src="./logo_index.png"
+             alt="头条新闻">
+      </div>
+
+      <div class="login_form">
+        <el-form :model="form">
+          <el-form-item>
+            <el-input v-model="form.mobile"
+                      placeholder="手机号"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-col :span='10'>
+              <el-input v-model="form.code"
+                        placeholder="验证码"></el-input>
+            </el-col>
+            <el-col :span='10'
+                    :offset='2'>
+              <el-button>获取验证码</el-button>
+            </el-col>
+          </el-form-item>
+          <el-form-item>
+            <el-button class="btn_login"
+                       type="primary"
+                       @click="onSubmit">登录</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,14 +37,49 @@
 export default {
   name: 'Applogin',
   data () {
-    return {}
+    return {
+      form: {
+        mobile: '19801118660',
+        code: '123456'
+      }
+    }
   },
-  components: {
-
+  methods: {
+    onSubmit () {
+      console.log('1')
+    }
   }
 }
 </script>
 
 <style lang="less" scoped>
+.login_wrap {
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #eee;
 
+  .login_form_wrap {
+    padding: 50px;
+    border-radius: 10px;
+    background-color: #fff;
+
+    .login_head {
+      display: flex;
+      justify-content: center;
+      margin-bottom: 20px;
+
+      img {
+        width: 200px;
+      }
+    }
+
+    .login_form {
+      .btn_login {
+        width: 100%;
+      }
+    }
+  }
+}
 </style>
